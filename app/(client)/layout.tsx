a1c05c9ee3ebce552a4f20b5d194f61df89382bb
Vosooghi.Group <vosooghi.group@gmail.com>
2024-11-13 15:49:55 +0330

    use next js Script tag

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Head from "next/head";
+import Script from "next/script";
 import { ThemeProvider } from "@/components/theme/theme-provier";
 import AppNavbar from "@/components/navigation/AppNavbar";
-// import { ConstructionBanner } from "@/components/ConstructionBanner";
 import { SessionProvider } from "next-auth/react";
 import { Footer } from "@/components/navigation/Footer";
 
@@ -34,25 +33,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <Head>
-        {/* Add your ad script here */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function (t, e, n) {
-                  t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
-                      t[n].q.push(arguments)
-                  }, t[n].q = t[n].q || [];
-                  var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
-                      c = e.getElementsByTagName("script")[0], s = e.createElement("script");
-                  s.id = "ua-script-0v2lE6Oq"; s.dataset.analyticsobject = n;
-                  s.async = 1; s.type = "text/javascript";
-                  s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/0v2lE6Oq/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c)
-              }(window, document, "yektanet");
-            `,
-          }}
-        />
-      </Head>
+      <Script
+        id="yektanet-analytics"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+      !function (t, e, n) {
+        t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
+            t[n].q.push(arguments)
+        }, t[n].q = t[n].q || [];
+        var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+            c = e.getElementsByTagName("script")[0], s = e.createElement("script");
+        s.id = "ua-script-0v2lE6Oq"; s.dataset.analyticsobject = n;
+        s.async = 1; s.type = "text/javascript";
+        s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/0v2lE6Oq/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c)
+      }(window, document, "yektanet");
+    `,
+        }}
+      />
       <body className={`antialiased`}>
         <SessionProvider>
           <ThemeProvider
